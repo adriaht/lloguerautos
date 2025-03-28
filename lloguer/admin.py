0abcacb6c200a517c962f10740bc7576f7f4460d
adriaht <adriah.t.22@gmail.com>
2025-03-28 15:59:57 +0100

    fita2

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from lloguer.models import Automobil
+
+
 
 # Register your models here.
+
+
+
+
+
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ( 'matricula','marca', 'model')  # Campos visibles en la lista
+    search_fields = ('marca', 'model', 'matricula')  # Campos de búsqueda
+
+
+admin.site.register(Automobil, AutomobilAdmin) 
